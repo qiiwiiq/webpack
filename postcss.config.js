4cab1ad309b14b9549beb33af78bfa8140c68613
PTT <qiiwiiq@hotmail.com>
2022-09-11 17:44:43 +0800

    add postcss

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require('autoprefixer')({
+      browsers: [
+        '> 1%',
+        'last 5 versions',
+        'Firefox >= 45',
+        'ios >= 8',
+        'Safari >= 8',
+        'ie >= 10',
+      ]
+    })
+  ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       },
     ]
   },
