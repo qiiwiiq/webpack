e1fd0cb635b86754e3812efc4f0723f37fdac7eb
PTT <qiiwiiq@hotmail.com>
2022-09-11 17:48:11 +0800

    move browserslist to package.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')({
-      browsers: [
-        '> 1%',
-        'last 5 versions',
-        'Firefox >= 45',
-        'ios >= 8',
-        'Safari >= 8',
-        'ie >= 10',
-      ]
-    })
+    require('autoprefixer')
   ]
 }
